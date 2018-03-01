43aa9b550ee0255aca3f9c593b0b3ba755da2ea3
Arne de Wall <arne@52north.org>
2018-03-01 15:10:05 +0000

    fixed build context

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
-#/bin/bash
+x#/bin/bash
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${scriptDir}"
 
 cd ../ && \
   mvn clean install && \
-  docker build -t ec4bit/adapter ./docker/
+  docker build -t ec4bit/adapter -f docker/Dockerfile .
 
