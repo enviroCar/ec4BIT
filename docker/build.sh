5682f89ad9e3730ce7d8cf8b0e24a15229ab97c7
Arne de Wall <arne@52north.org>
2018-03-07 10:33:58 +0000

    skip tests during maven build

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${scriptDir}"
 
 cd ../ && \
-  mvn clean install && \
+  mvn clean install -DskipTests && \
   docker build -t ec4bit/adapter -f docker/Dockerfile .
 
